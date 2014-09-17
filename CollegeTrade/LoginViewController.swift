995b261309688c1191c1f7605bbf6f46072fe192
Shaoqing Yang <ysqvision@gmail.com>
2014-09-16 21:26:57 -0400

    Transition

---
+++ CollegeTrade/LoginViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CollegeTrade
+//
+//  Created by Shaoqing Yang on 9/16/14.
+//  Copyright (c) 2014 Shaoqing Yang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBAction func validateLoginCredentials(sender: AnyObject) {
+        var username = usernameTextField.text
+        var password = passwordTextField.text
+        var pass = checkLoginCredential(username, password: password)
+        if pass {
+            performSegueWithIdentifier("LoginSuccessful", sender: self)
+        }
+    }
+    
+    func checkLoginCredential(username: String, password: String) -> Bool {
+        // Check if username and password pass
+        if username == "victor" && password == "pass" {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        passwordTextField.secureTextEntry = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- CollegeTrade/UserHomePageViewController.swift
+++ CollegeTrade/UserHomePageViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UserHomePageViewController.swift
 //  CollegeTrade
 //
 //  Created by Shaoqing Yang on 9/16/14.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class UserHomePageViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("here")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
