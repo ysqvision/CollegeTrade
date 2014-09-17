6a6cd760af754dd69cb7abab7afac77479c56f5a
Shaoqing Yang <ysqvision@gmail.com>
2014-09-16 22:09:48 -0400

    Added item detail view

--- CollegeTrade/UserHomePageViewController.swift
+++ CollegeTrade/UserHomePageViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class UserHomePageViewController: UIViewController {
+class UserHomePageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let kCellIdentifier: String = "ItemForSellCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +22,21 @@ class UserHomePageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("ItemForSellCell") as UITableViewCell
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+     
+    }
     
 }
