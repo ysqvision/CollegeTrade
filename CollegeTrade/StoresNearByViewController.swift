74e5ffbc6a65ce44c584a6847f96fe2706a5f5b4
Shaoqing Yang <ysqvision@gmail.com>
2014-10-06 22:21:28 -0400

    random

---
+++ CollegeTrade/StoresNearByViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StoresNearByViewController.swift
+//  CollegeTrade
+//
+//  Created by Shaoqing Yang on 10/6/14.
+//  Copyright (c) 2014 Shaoqing Yang. All rights reserved.
+//
+import UIKit
+
+class StoresNearByViewController : UIViewController {
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
