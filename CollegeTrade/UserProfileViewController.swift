fa70bdf5abe034010630caf3794abb5089e708fc
maojm <maojm517@gmail.com>
2014-10-06 20:47:57 -0400

    gaomei
    gaomei

---
+++ CollegeTrade/UserProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserProfileViewController.swift
+//  CollegeTrade
+//
+//  Created by Jieming Mao on 14-10-6.
+//  Copyright (c) 2014年 Shaoqing Yang. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
