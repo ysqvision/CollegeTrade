a0f1debaaa1ad28bb0be2fe906e6e4fd276eea03
Shaoqing Yang <ysqvision@gmail.com>
2014-10-06 22:23:13 -0400

    Revert 74e5ffb..9f1fe41
    This rolls back to commit 74e5ffbc6a65ce44c584a6847f96fe2706a5f5b4.

--- CollegeTrade/UserHomePageViewController.swift
+++ CollegeTrade/UserHomePageViewController.swift
@@ -12,7 +12,6 @@ class UserHomePageViewController: UIViewController, UITableViewDataSource, UITab
     let kCellIdentifier: String = "ItemForSellCell"
     var itemsForSell = [ItemForSell]()
     
-    @IBOutlet weak var transition: UIButton!
     @IBOutlet weak var itemsTable: UITableView?
     override func viewDidLoad() {
         super.viewDidLoad()
