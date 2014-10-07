9badc556e07e0f3c8962460c8a720e12a8c2838c
maojm <maojm517@gmail.com>
2014-10-06 22:21:40 -0400

    afdas
    adfsa

--- CollegeTrade/UserProfileViewController.swift
+++ CollegeTrade/UserProfileViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
     
+    @IBOutlet weak var userProfileButton: UIButton!
+    @IBOutlet weak var shopNameButton: UIButton!
+    @IBOutlet weak var myItemButton: UIButton!
+    @IBOutlet weak var boughtItemButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userProfileButton.layer.borderWidth = 1
+        shopNameButton.layer.borderWidth = 1
+        myItemButton.layer.borderWidth = 1
+        boughtItemButton.layer.borderWidth = 1
+        settingButton.layer.borderWidth = 1
         
         // Do any additional setup after loading the view, typically from a nib.
     }
