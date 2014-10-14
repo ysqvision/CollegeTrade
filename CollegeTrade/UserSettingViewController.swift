85fc6afa0f520ddf6339c7b6dc75034855ff0212
maojm <maojm517@gmail.com>
2014-10-13 23:02:22 -0400

    cell

--- CollegeTrade/UserSettingViewController.swift
+++ CollegeTrade/UserSettingViewController.swift
@@ -12,11 +12,31 @@ class UserSettingViewController : UIViewController {
     @IBAction func goBackFromUserSetting(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    @IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.table.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
+    
+    func table(table: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3;
+    }
+    
+    func table(table: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.table.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = "杨少卿"
+        
+        return cell
+    }
+    
+    func table(table: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
