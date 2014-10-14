a4d8ed012bd8cbdc6bf745910fa37b810c598848
maojm <maojm517@gmail.com>
2014-10-14 00:15:16 -0400

    shouzhi

--- CollegeTrade/UserSettingViewController.swift
+++ CollegeTrade/UserSettingViewController.swift
@@ -12,35 +12,28 @@ class UserSettingViewController : UIViewController {
     @IBAction func goBackFromUserSetting(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
-    @IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.table.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
-    func table(table: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3;
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
     
-    func table(table: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell:UITableViewCell = self.table.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
         
-        cell.textLabel?.text = "杨少卿"
+        cell.textLabel?.text = "杨手指"
+        cell.detailTextLabel?.text = "杨少卿"
         
         return cell
     }
     
-    func table(table: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
-        println("You selected cell #\(indexPath.row)!")
-    }
-
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
     
     
