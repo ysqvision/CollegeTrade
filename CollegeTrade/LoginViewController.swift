13c62db1544576981e6d008bfa304d11fa0d7b01
Shaoqing Yang <ysqvision@gmail.com>
2014-10-14 00:15:26 -0400

    post request for login and signup

--- CollegeTrade/LoginViewController.swift
+++ CollegeTrade/LoginViewController.swift
@@ -30,6 +30,50 @@ class LoginViewController: UIViewController {
     func checkLoginCredential(username: String, password: String) -> Bool {
         
         // Check if username and password pass
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://14.29.65.186:9090/SpiriiitTradeServer/user-login"))
+        var session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        
+        var params = ["username":"鹏程", "password":"asdadadas"] as Dictionary<String, String>
+        
+        var err: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        print(NSString(data:request.HTTPBody!, encoding: NSUTF8StringEncoding))
+      
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            println("Response: \(response)")
+            println(error)
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("Body: \(strData)")
+            var err: NSError?
+            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            if(err != nil) {
+                println(err!.localizedDescription)
+                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Error could not parse JSON: '\(jsonStr)'")
+            }
+            else {
+                // The JSONObjectWithData constructor didn't return an error. But, we should still
+                // check and make sure that json has a value using optional binding.
+                if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    var success = parseJSON["Status"] as? Int
+                    println("Succes: \(success)")
+                }
+                else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("Error could not parse JSON: \(jsonStr)")
+                }
+            }
+        })
+        
+        task.resume()
+
         if username == "victor" && password == "pass" {
             KeychainService.saveToken(username, token: password)
             return true;
--- CollegeTrade/SignUpViewController.swift
+++ CollegeTrade/SignUpViewController.swift
@@ -32,21 +32,53 @@ class SignUpViewController: UIViewController {
             passwordTextField.text = "";
             repeatPasswordTextField.text = "";
         }
-        else if pass {
+        else {
+            var request = NSMutableURLRequest(URL: NSURL(string: "http://14.29.65.186:9090/SpiriiitTradeServer/user-register"))
+            var session = NSURLSession.sharedSession()
+            request.HTTPMethod = "POST"
             
+            var params = ["username":"username", "password":"password", "UserImage": "imageFile", "SchoolId": "s", "NickName": "nickname"] as Dictionary<String, String>
+            
+            var err: NSError?
+            request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+                println("Response: \(response)")
+                println(error)
+                var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Body: \(strData)")
+                var err: NSError?
+                var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+                
+                // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+                if(err != nil) {
+                    println(err!.localizedDescription)
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("Error could not parse JSON: '\(jsonStr)'")
+                }
+                else {
+                    // The JSONObjectWithData constructor didn't return an error. But, we should still
+                    // check and make sure that json has a value using optional binding.
+                    if let parseJSON = json {
+                        // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                        var success = parseJSON["Status"] as? Int
+                        println("Succes: \(success)")
+                    }
+                    else {
+                        // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                        let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                        println("Error could not parse JSON: \(jsonStr)")
+                    }
+                }
+            })
+            
+            task.resume()
             dismissViewControllerAnimated(true, completion: nil)
             
         }
-        else {
-            let myAlert = UIAlertView(title: "错误",
-                message: "用户名已经注册",
-                delegate: nil, cancelButtonTitle: "重新输入")
-            myAlert.show()
-            
-            usernameTextField.text = "";
-            passwordTextField.text = "";
-            repeatPasswordTextField.text = "";
-        }
+       
 
     }
     
