fbef5f8a3eb55fcb8572781fc7326e92d222f611
maojm <maojm517@gmail.com>
2014-10-28 01:48:47 -0400

    a
    a

--- CollegeTrade/LoginViewController.swift
+++ CollegeTrade/LoginViewController.swift
@@ -12,6 +12,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
     
+    @IBOutlet weak var loginButton: UIButton!
     
     @IBAction func validateLoginCredentials(sender: AnyObject) {
         var username = usernameTextField.text
@@ -38,9 +39,45 @@ class LoginViewController: UIViewController {
     
         
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
         passwordTextField.secureTextEntry = true
         // Do any additional setup after loading the view, typically from a nib.
+        
+       // let lg = UIButton.buttonWithType(UIButtonType.System) as UIButton
+       // lg.frame = CGRectMake(50,400,200,50)
+       // lg.backgroundColor = UIColor.blueColor()
+       // lg.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+       // lg.setTitle("也是登陆", forState: UIControlState.Normal)
+       // lg.addTarget(self, action:"loginAction:", forControlEvents: UIControlEvents.TouchUpInside)
+       // self.view.addSubview(lg)
+    }
+    
+    func loginAction(sender:UIButton!)
+    {
+        var username = usernameTextField.text
+        var password = passwordTextField.text
+        DataBaseAPIHelper.checkLoginCredential(username, password: password) { (success: Bool) -> () in
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if success {
+                    
+                    USER_IS_LOGGED_IN = true
+                    KeychainService.saveToken("SPIRIIITCOLLEGETRADEUSERNAME", token: username)
+                    KeychainService.saveToken("SPIRIIITCOLLEGETRADEPASSWORD", token: password)
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                    
+                } else {
+                    
+                    var myAlert = UIAlertView(title: "非法登陆",
+                        message: "请输入正确的用户名和密码",
+                        delegate: nil, cancelButtonTitle: "重新输入")
+                    myAlert.show()
+                }
+            })
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
