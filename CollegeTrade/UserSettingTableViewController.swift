e871f9fc3f2cac7ae254aef01085bea49399d03b
maojm <maojm517@gmail.com>
2014-10-28 22:09:33 -0400

    a
    a

---
+++ CollegeTrade/UserSettingTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  UserSettingTableViewController.swift
+//  CollegeTrade
+//
+//  Created by Jieming Mao on 14/10/28.
+//  Copyright (c) 2014年 Shaoqing Yang. All rights reserved.
+//
+
+import UIKit
+
+class UserSettingTableViewController: UITableViewController {
+}
