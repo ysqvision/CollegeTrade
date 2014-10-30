1f67a3e24844ce497097cf7b44d06346757b85b3
Shaoqing Yang <ysqvision@gmail.com>
2014-10-30 00:54:55 -0400

    asdfsd

---
+++ CollegeTrade/StoreHomePageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StoreHomePageViewController.swift
+//  CollegeTrade
+//
+//  Created by Shaoqing Yang on 10/30/14.
+//  Copyright (c) 2014 Shaoqing Yang. All rights reserved.
+//
+
+import UIKit
+
+class StoreHomePageViewController: UIViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+}
